6de747d18c674637ddd1631fdc5755ab0c1ea7cc
DESKTOP-OHSHGG1\mikec <michael.freeman.ext@lilium.com>
2021-06-19 11:08:56 +0100

    Initial Commit

---
+++ src/GraphQL/BookType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GraphQL.Types;
+
+namespace AspNetCoreGraphQL.GraphQL
+{
+    public class BookType : ObjectGraphType<Book>
+    {
+        public BookType()
+        {
+            Field(x => x.Id);
+            Field(x => x.Title);
+            Field(x => x.Pages, nullable: true);
+            Field(x => x.Chapters, nullable: true);
+        }
+    }
+}
---
+++ src/GraphQL/GraphSchema.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using GraphQL.Types;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AspNetCoreGraphQL.GraphQL
+{
+    public class GraphSchema : Schema
+    {
+        public GraphSchema(IServiceProvider provider) : base(provider)
+        {
+            Query = provider.GetRequiredService<RootQuery>();
+        }
+    }
+}
